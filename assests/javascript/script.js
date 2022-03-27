e1e581f2003da1994e4113dde58096c5d7c6b247
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-03-27 19:57:21 -0400

    made changes to html and made it office theme

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -6,22 +6,26 @@ var screen1El = document.querySelector("#screen1");
 var screen1BtnEl = screen1El.querySelector("#button");
 var screen2El = document.querySelector("#screen2");
 var screen2BtnEl = screen2El.querySelector("#button");
-var doSomethingEl = document.querySelector("#doSomething");
+var screen3El = document.querySelector("#screen3");
+var screen3BtnEl = screen3El.querySelector("#button");
+var highScoresEl = document.querySelector("#HighScores");
 var saySomethingEl = document.querySelector("#saySomething");
 var questionsEl = document.querySelector("#questions");
 var answersEl = document.querySelector("possibleAnswers");
 
+
 var HIDE_CLASS = "isHidden";
+
 var questions = [
     {
         question: " Who is the manager of the Scranton Branch of Dunder Mifflin ",
         answers: ["Andy Bernard", "Dwight Schrute", "Michael Scott", "Jim Halpert"],
-        answer: 0
+        answer: 2
     },
     {
         question: " What special package does Packman leave in Michael's office? ",
         answers: ["Cake", "Stray Dogs", "Poop", "His stapler in jello"],
-        answer: 0
+        answer: 2
     },
     {
         question: " What song does Michael play on his way to his new paper company? ",
@@ -31,12 +35,12 @@ var questions = [
     {
         question: " What is Andy's nickname from college ",
         answers: ["Nard Dog", "Wicked Hammer", "Buzz", "Ace"],
-        answer: 0
+        answer: 1
     },
     {
         question: " Why is Jim good at basketball? ",
         answers: ["He is 6'3", "Ball is life", "Played in College", "It was his thing in high school"],
-        answer: 0
+        answer: 3
     },
     {
         question: " Why is Michael depressed and is going to jump off the roof? ",
@@ -46,22 +50,22 @@ var questions = [
     {
         question: " Why did Jan set up a camera in her and Michael's bedroom? ",
         answers: ["To post the video online", "To show her therapist", "For memories", "To show Michael where he needed to fix his form"],
-        answer: 0
+        answer: 1
     },
     {
         question: " How big is Michael's Plasma TV? ",
         answers: ["80in", "50in", "30in", "14in"],
-        answer: 0
+        answer: 3
     },
     {
         question: " What did Oscar and Kevin catch Angela doing on her nanny cam? ",
         answers: ["Sleeping with her cats", "Feeding her cats dog food", "Licking her cats clean", "Smuggling Cats"],
-        answer: 0
+        answer: 2
     },
     {
         question: " Who does Michael hit with his car on the way to work? ",
         answers: ["Dwight", "Meredith", "Kevin", "Kelly"],
-        answer: 0
+        answer: 1
     },
 ];
 var currentQuestion = 0;
@@ -70,7 +74,7 @@ var dynamicElements = [
     screen0El,
     screen1El,
     screen2El,
-    doSomethingEl,
+    highScoresEl,
     saySomethingEl,
 ];
 
@@ -134,6 +138,7 @@ function setEventListeners() {
 }
 
 init();
+
 const updateTimer = () =>{
     setInterval(()=>{
         let timer = document.getElementById('timer');
